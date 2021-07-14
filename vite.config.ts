6ae0d2ecf25d55531ae257c27bf2b6b7eb836967
Bowen Wu <bwwu@iflytek.com>
2021-07-14 14:28:31 +0800

    feat: add global scss & global import

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,23 @@ import DevConfig from './config/vite.config.dev';
 import ProdConfig from './config/vite.config.prod';
 
 export default ({ command, mode }: { mode: string, command: string; }) => {
-  console.log(command);
-  console.log(mode);
+    console.log(command);
+    console.log(mode);
 
-  const config = mode === 'production' ? ProdConfig : DevConfig;
+    const config = mode === 'production' ? ProdConfig : DevConfig;
 
-  return defineConfig({
-    plugins: [ reactRefresh() ],
-    define: {
-      __PRODUCT_SUGGEST_URL__: JSON.stringify(''),
-      ...config.define
-    }
-  });
+    return defineConfig({
+        plugins: [ reactRefresh() ],
+        define: {
+            __PRODUCT_SUGGEST_URL__: JSON.stringify(''),
+            ...config.define
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    additionalData: '@import ./src/assets/styles/_variable.scss; @import ./src/assets/styles/_mixin.scss; @import ./src/assets/styles/_zIndex.scss; @import ./src/assets/styles/_function.scss;'
+                }
+            }
+        }
+    });
 }
