b58bb24c47c5b3259f31ead5fdc7eefd1e621d85
bowen-wu <13793248162@163.com>
2021-07-29 15:16:58 +0800

    feat: add uniform code style

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:bowen-lint/reactLint'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'never'],
+    'react/jsx-indent': ['error', 2],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'react/jsx-tag-spacing': [
+      'error',
+      {
+        closingSlash: 'never',
+        beforeSelfClosing: 'allow',
+        afterOpening: 'never',
+        beforeClosing: 'allow'
+      }
+    ],
+    'object-curly-spacing': ['warn', 'never'],
+    'react/jsx-indent-props': ['off', 'tab'],
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error']
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf'
+};
