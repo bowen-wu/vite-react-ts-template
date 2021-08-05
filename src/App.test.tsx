540af87d8938fa51660e1466a93de09f9a7a3da3
bowen-wu <13793248162@163.com>
2021-08-05 15:21:30 +0800

    feat: add App.test.tsx snapshot

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,11 @@ import App from './App';
 describe('Test App router', () => {
   afterEach(cleanup);
 
+  it('take a snapshot', () => {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   it('full app rendering/navigating', () => {
     const { container } = render(<App />);
     expect(container.querySelector('div.App')).toBeTruthy();
