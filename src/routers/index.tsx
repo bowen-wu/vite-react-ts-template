5ed244756013a29c4e00dd512a2335936163038b
bowen-wu <13793248162@163.com>
2021-09-13 14:12:37 +0800

    fix: fixed router unique key

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -7,10 +7,13 @@ const renderRouter = (routers: RouteInterface[]) => {
   return (
     <Fragment>
       {routers.map((router: RouteInterface) => {
+        const key = router.name ? `${router.path}-${router.name}` : router.path;
+
         if (router.routes && router.routes.length > 0) {
-          return renderRouter(router.routes);
+          return <div key={key}>{renderRouter(router.routes)}</div>;
         }
-        return <Route exact key={router.path} path={router.path} component={router.component} />;
+
+        return <Route exact key={key} path={router.path} component={router.component} />;
       })}
     </Fragment>
   );
